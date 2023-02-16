1196726b61f7276dd56c9dad41f1661e23266fab
LeeGingko <1596510773@qq.com>
2023-02-16 20:00:28 +0800

    添加.gitignore
    Signed-off-by: LeeGingko <1596510773@qq.com>

--- Objects/segger_rtt.d
+++
@@ -1,6 +0,0 @@
-.\objects\segger_rtt.o: SEGGER_RTT_V764b\RTT\SEGGER_RTT.c
-.\objects\segger_rtt.o: SEGGER_RTT_V764b\RTT\SEGGER_RTT.h
-.\objects\segger_rtt.o: .\SEGGER_RTT_V764b\Config\SEGGER_RTT_Conf.h
-.\objects\segger_rtt.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\objects\segger_rtt.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
-.\objects\segger_rtt.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
